3b6385a65fe157141af5c3745e400027b52c1d2c
nghiangodinh <you@example.com>
2017-12-20 09:33:40 +0700

    using native File to save image from camera

--- src/pages/place-add/place-add.ts
+++ src/pages/place-add/place-add.ts
@@ -1,7 +1,8 @@
-import { PlaceService } from './../../services/place';
+import { PlaceService } from "./../../services/place";
 import { Component } from "@angular/core";
 import { Geolocation } from "@ionic-native/geolocation";
 import { Camera, CameraOptions } from "@ionic-native/camera";
+import { File, Entry, FileError } from "@ionic-native/file";
 
 import {
   ModalController,
@@ -11,6 +12,8 @@ import {
 import { NgForm } from "@angular/forms";
 import { LocationSetPage } from "../pages";
 import { Location } from "../../models/location";
+
+declare var cordova: any;
 
 @Component({
   selector: "page-place-add",
@@ -30,6 +33,7 @@ export class PlaceAddPage {
     private toastCtrl: ToastController,
     private geolocation: Geolocation,
     private camera: Camera,
+    private file: File,
     private placeService: PlaceService
   ) {}
 
@@ -80,16 +84,41 @@ export class PlaceAddPage {
 
     this.camera.getPicture(options).then(
       imageData => {
+        const currentName = imageData.replace(/^.*[\\\/]/, '');
+        const path = imageData.replace(/[^\/]*$/, '');
+        this.file.moveFile(path, currentName, cordova.file.dataDirectory, currentName)
+          .then((data: Entry) => {
+            this.imageUrl = data.nativeURL;
+            this.camera.cleanup();
+            //this.file.removeFile(path, currentName);
+          })
+          .catch((err: FileError) => {
+            const toast = this.toastCtrl.create({
+              message: "Could not save the image. Please try again!",
+              duration: 2500
+            });
+            toast.present();
+            this.camera.cleanup();
+          });
         this.imageUrl = imageData;
       },
       err => {
-        console.log(err);
+        const toast = this.toastCtrl.create({
+          message: "Could not take the image. Please try again!",
+          duration: 2500
+        });
+        toast.present();
       }
     );
   }
 
   onSubmit(form: NgForm) {
-    this.placeService.addPlace(form.value.title, form.value.description, this.location, this.imageUrl);
+    this.placeService.addPlace(
+      form.value.title,
+      form.value.description,
+      this.location,
+      this.imageUrl
+    );
 
     // reset form & model
     form.reset();
